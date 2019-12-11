6f7f62b46af2caf8090abe2003aec8ed2438283a
MinexAutomation <minex.automation@gmail.com>
2019-12-11 12:20:32 -0800

    An ISolutionFileNameProvider (really solution file selector) service implemenation that provides the solution file-name it was provided with at construction.

---
+++ source/R5T.Pompeii.Standard/Code/Services/Classes/DirectSolutionFileNameProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace R5T.Pompeii.Standard
+{
+    public class DirectSolutionFileNameProvider : ISolutionFileNameProvider
+    {
+        private string SolutionFileName { get; }
+
+        
+        public DirectSolutionFileNameProvider(string solutionFileName)
+        {
+            this.SolutionFileName = solutionFileName;
+        }
+
+        public string GetSolutionFileName(string solutionDirectoryPath)
+        {
+            return this.SolutionFileName;
+        }
+    }
+}
