a448ec1b8477c6c979cc9c4168dcb60e37d47b87
MinexAutomation <minex.automation@gmail.com>
2020-02-16 22:55:51 -0800

    Removed service definitions moved to R5T.Pompeii.Default.

--- source/R5T.Pompeii.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Pompeii.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,11 +2,93 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
+using R5T.Lombardy;
+using R5T.Macommania;
+
+using R5T.Pompeii.Default;
+
 
 namespace R5T.Pompeii.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        #region Standard Aggregations
+
+        /// <summary>
+        /// Adds the <see cref="ISolutionAndProjectFileSystemConventions"/> service.
+        /// </summary>
+        public static IServiceCollection AddSolutionAndProjectFileSystemConventions(this IServiceCollection services,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            services.AddStandardSolutionAndProjectFileSystemConventions(addStringlyTypedPathOperator);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ISolutionAndProjectFileSystemConventions"/> service.
+        /// </summary>
+        public static ServiceAction<ISolutionAndProjectFileSystemConventions> AddSolutionAndProjectFileSystemConventionsAction(this IServiceCollection services,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<ISolutionAndProjectFileSystemConventions>(() => services.AddSolutionAndProjectFileSystemConventions(
+                addStringlyTypedPathOperator));
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IEntryPointProjectNameProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddEntryPointProjectNameProvider(this IServiceCollection services, string entryPointProjectName)
+        {
+            services.AddDirectEntryPointProjectNameProvider(entryPointProjectName);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IEntryPointProjectNameProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IEntryPointProjectNameProvider> AddEntryPointProjectNameProviderAction(this IServiceCollection services, string entryPointProjectName)
+        {
+            var serviceAction = new ServiceAction<IEntryPointProjectNameProvider>(() => services.AddEntryPointProjectNameProvider(entryPointProjectName));
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IEntryPointProjectFilePathProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddEntryPointProjectDirectoryPathProvider(this IServiceCollection services,
+            ServiceAction<IEntryPointProjectDirectoryPathProvider> addEntryPointProjectDirectoryPathProvider,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            services.AddSingleProjectInDirectoryEntryPointProjectFilePathProvider(
+                addEntryPointProjectDirectoryPathProvider,
+                addStringlyTypedPathOperator);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IEntryPointProjectFilePathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IEntryPointProjectFilePathProvider> AddEntryPointProjectDirectoryPathProviderAction(this IServiceCollection services,
+            ServiceAction<IEntryPointProjectDirectoryPathProvider> addEntryPointProjectDirectoryPathProvider,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<IEntryPointProjectFilePathProvider>(() => services.AddEntryPointProjectDirectoryPathProvider(
+                addEntryPointProjectDirectoryPathProvider,
+                addStringlyTypedPathOperator));
+            return serviceAction;
+        }
+
+        #endregion
+
+
+
+
+
         public static IServiceCollection UseStandardEntryPointProjectConventions(this IServiceCollection services, string entryPointProjectName, string buildConfigurationName)
         {
             services
